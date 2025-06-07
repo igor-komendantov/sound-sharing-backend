aeba0e9e8f25225610f2a341ba1ce95e40ce97a8
Igor Komendantov <komendantov2580@gmail.com>
2025-06-07 14:05:00 +0300

    fix docker's errors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ClientDatabaseService } from 'src/client-database/client-database.service';
 
 @Controller('auth')
