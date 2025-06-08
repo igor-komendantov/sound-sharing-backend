7a710f7235cd8eba90965aa59c45dd434145af4b
Igor Komendantov <komendantov2580@gmail.com>
2025-06-08 19:04:39 +0300

    add fake auth, attach user to requests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/authentication/current-user.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@CurrentUser() user) {
+    return user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ClientDatabaseModule } from './client-database/client-database.module';
+import { FakeAuthMiddleware } from './auth/authentication/fake-auth.middleware';
 
 @Module({
   imports: [ClientDatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(FakeAuthMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
