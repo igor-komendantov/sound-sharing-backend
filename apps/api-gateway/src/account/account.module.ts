5bc0f7ac2b78dcdb38d880b26d2b874228ea63b7
Igor Komendantov <komendantov2580@gmail.com>
2025-06-16 19:11:19 +0300

    host account app on 3002

--- apps/api-gateway/src/account/account.module.ts
+++ apps/api-gateway/src/account/account.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   controllers: [AccountController],
   providers: [AccountService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ACCOUNT_CLIENT',
+        transport: Transport.TCP,
+        options: { port: 3002 },
+      },
+    ]),
+  ],
 })
 export class AccountModule {}
