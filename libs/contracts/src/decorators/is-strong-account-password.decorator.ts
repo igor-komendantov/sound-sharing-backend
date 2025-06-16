30df0212266e43174143dd88d923f9b325960891
Igor Komendantov <komendantov2580@gmail.com>
2025-06-16 21:31:46 +0300

    edit config in IsStrongAccountPassword

--- libs/contracts/src/decorators/is-strong-account-password.decorator.ts
+++ libs/contracts/src/decorators/is-strong-account-password.decorator.ts
@@ -2,5 +2,13 @@ import { applyDecorators } from '@nestjs/common';
 import { IsStrongPassword } from 'class-validator';
 
 export function IsStrongAccountPassword() {
-  return applyDecorators(IsStrongPassword({ minLength: 8 }));
+  return applyDecorators(
+    IsStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 0,
+      minNumbers: 1,
+      minSymbols: 0,
+    }),
+  );
 }
